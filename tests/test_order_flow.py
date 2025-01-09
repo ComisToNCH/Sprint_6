d889a7df6f2f8057e6c94776896962ccc4e198ce
ComisToNCH <raspberrygloom@ya.ru>
2025-01-09 17:03:12 +0300

    вынесла DTO в src/data и обновила тест на заказ

--- tests/test_order_flow.py
+++ tests/test_order_flow.py
@@ -1,52 +1,13 @@
 import pytest
-from dataclasses import dataclass
+from src.data.order import ORDER_DATA_LIST
 
-@dataclass
-class OrderTestData:
-    name: str
-    surname: str
-    address: str
-    metro: str
-    phone: str
-    date: str
-    duration_text: str
-    color_black: bool
-    color_grey: bool
-    comment: str
-
-@pytest.mark.parametrize("test_data", [
-    OrderTestData(
-        name="Иван",
-        surname="Иванов",
-        address="Москва",
-        metro="Сокол",
-        phone="89990001122",
-        date="02.02.2025",
-        duration_text="трое суток",
-        color_black=True,
-        color_grey=False,
-        comment="Позвонить за час"
-    ),
-    OrderTestData(
-        name="Мария",
-        surname="Петрова",
-        address="Москва",
-        metro="Бабушкинская",
-        phone="88888888888",
-        date="03.03.2025",
-        duration_text="двое суток",
-        color_black=False,
-        color_grey=True,
-        comment="Доставить к подъезду"
-    )
-])
+@pytest.mark.parametrize("test_data", ORDER_DATA_LIST)
 def test_order_positive_flow(order_page, test_data):
     """
     Тест позитивного сценария заказа самоката.
-    Параметры входных данных передаются одним объектом (DTO).
+    Берём набор данных из ORDER_DATA_LIST (src/data/order.py).
     """
-
-    # 3. Заполнить первую часть формы
+    # 1. Заполнить первую часть формы
     order_page.fill_personal_data(
         test_data.name,
         test_data.surname,
@@ -56,7 +17,7 @@ def test_order_positive_flow(order_page, test_data):
     )
     order_page.click_next_button()
 
-    # 4. Заполнить вторую часть
+    # 2. Заполнить вторую часть
     order_page.fill_rent_data(
         test_data.date,
         test_data.duration_text,
@@ -66,10 +27,10 @@ def test_order_positive_flow(order_page, test_data):
     )
     order_page.click_order_button()
 
-    # 5. Подтвердить заказ
+    # 3. Подтвердить заказ
     order_page.confirm_order_modal()
 
-    # 6. Проверить, что заказ оформлен
+    # 4. Проверить, что заказ оформлен
     success_text = order_page.get_success_message()
     assert "Заказ оформлен" in success_text, (
         f"Не найдено подтверждение заказа. Текст: {success_text}"
